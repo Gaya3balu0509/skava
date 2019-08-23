c7a3cb82412450869ddd38758691e64182e8792a
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-08-23 10:42:45 +0530

    Create balancedparanthesis.java

---
+++ balancedparanthesis.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*; 
+public class Main {
+     public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int j=0;j<n;j++)
+        {
+        String str=in.next();
+         Stack st = new Stack<>();
+         for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == '{' || str.charAt(i) == '[' || str.charAt(i) == '(') 
+            {
+                   st.push(str.charAt(i));
+            } 
+else if ( !st.empty() && ((str.charAt(i) == ']' |(str.charAt(i) == '}')||(str.charAt(i) == ')'))))
+       { st.pop();
+ 
+             } else {
+                  st.push(str.charAt(i));
+             }
+         }
+ 
+         if(st.empty()) {
+              System.out.println("YES");
+         } else {
+              System.out.println("NO");
+         }
+   }}
+}
