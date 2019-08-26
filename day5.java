1c57dd963780b0f473ab727cf6b42c5683a00e3a
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-08-26 16:09:19 +0530

    Create day5.java

---
+++ day5.java
@@ -0,0 +1,16 @@
+//question link   https://www.hackerrank.com/challenges/30-loops/problem
+import java.io.*;
+import java.util.*;
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+    Scanner sc = new Scanner(System.in);
+    int N=sc.nextInt();
+    for(int i=1;i<11;i++)
+    {
+          System.out.println(N +" x "+ i+" = "+(N*i));
+    }	        
+    }
+}
+    
