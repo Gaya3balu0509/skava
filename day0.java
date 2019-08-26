6f28132224c6a37079981016ed76ab61fc2482c5
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-08-26 14:46:57 +0530

    Create day0.java

---
+++ day0.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+        String input = scan.nextLine(); 
+        System.out.println("Hello, World.");
+        System.out.println(input);
+        }}
