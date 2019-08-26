d09c968fb4882c02f27ceaea7ccb6b06a320303a
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-08-26 15:30:29 +0530

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+	    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";		
+        Scanner scan = new Scanner(System.in);
+         Integer inte=scan.nextInt();
+         Double doubl=scan.nextDouble();
+         String str=scan.next();
+         str+=scan.nextLine();
+        System.out.println(i+inte);
+        System.out.println(d+doubl);
+        System.out.println(s+str);
+        scan.close();
+    }
+}
