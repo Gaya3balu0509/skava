eaa0168c444ec28ed2fe3ff5df64ecb7148eb718
Gaya3balu0509 <46029857+Gaya3balu0509@users.noreply.github.com>
2019-09-03 09:49:40 +0530

    Create day20.java

---
+++ day20.java
@@ -0,0 +1,28 @@
+//question link   https://www.hackerrank.com/challenges/30-sorting/problem
+import java.io.*;
+import java.util.*;
+public class Main
+{  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len=sc.nextInt();
+        int temp,count=0;
+        int [] n=new int[len]; 
+         for(int i=0;i<len;i++)
+        {
+            int a=sc.nextInt();
+             n[i]=a;  
+        }
+          for(int i=0;i<len;i++)
+        { 
+            for(int j=0;j<len-i-1;j++)
+        {  
+            if (n[j]>n[j+1])
+        {   count++;
+            temp=n[j];
+            n[j]=n[j+1];
+            n[j+1]=temp;
+        }}}
+        System.out.println("Array is sorted in "+count+ " swaps.");
+        System.out.println("First Element: "+n[0]); 
+        System.out.println("Last Element: "+n[len-1]); 
+         }}
